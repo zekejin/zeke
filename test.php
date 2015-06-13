5967156afe5c4d6f4d1916294dc0844e5ae3bd18
zeke <2286649698@qq.com>
2015-06-14 00:54:27 +0800

    add

--- test.php
+++ test.php
@@ -13,3 +13,18 @@ $val = '0';
 //echo(isset($val))?'a':'b';
 
 echo "test";
+
+echo"hello world!!";
+
+
+$val = '0';
+//echo(isset($val))?'a':'b';
+
+echo "test";
+echo"hello world!!";
+
+
+$val = '0';
+//echo(isset($val))?'a':'b';
+
+echo "test";
